66c402d7d93efa7a8e931fe5593d2ca9f38d57cb
sumanstm21 <sumanstm21@gmail.com>
2024-09-07 15:52:23 +0200

    Webhook

--- forms/contact.py
+++ forms/contact.py
@@ -4,7 +4,8 @@ import streamlit as st
 import requests  # pip install requests
 
 
-WEBHOOK_URL = st.secrets["WEBHOOK_URL"]
+#WEBHOOK_URL = st.secrets["WEBHOOK_URL"]
+WEBHOOK_URL = "https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjYwNTZkMDYzNDA0MzQ1MjZmNTUzZDUxM2Ei_pc"
 
 
 def is_valid_email(email):
